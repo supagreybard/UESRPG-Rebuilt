54076cc0e67d4783b3c2268400ab584d45a61857
James Johnes <greybard@protonmail.com>
2026-04-04 14:40:11 -0400

    fix deprecation warnings

--- src/module/applications/index.ts
+++ src/module/applications/index.ts
@@ -8,22 +8,32 @@ import { NPCSheet } from './actor/NPCSheet';
 import { UesrpgItemSheet } from './item/UesrpgItemSheet';
 
 export function registerApplicationClasses(): void {
-  Actors.unregisterSheet('core', ActorSheet);
-  Items.unregisterSheet('core', ItemSheet);
+  const { DocumentSheetConfig } = foundry.applications.apps;
 
-  Actors.registerSheet(SYSTEM_ID, CharacterSheet as any, {
+  DocumentSheetConfig.unregisterSheet(
+    Actor,
+    'core',
+    foundry.appv1.sheets.ActorSheet,
+  );
+  DocumentSheetConfig.unregisterSheet(
+    Item,
+    'core',
+    foundry.appv1.sheets.ItemSheet,
+  );
+
+  DocumentSheetConfig.registerSheet(Actor, SYSTEM_ID, CharacterSheet as any, {
     types: [ACTOR_DOCUMENT_TYPES[0]],
     makeDefault: true,
     label: 'UESRPG.Sheets.character',
   });
 
-  Actors.registerSheet(SYSTEM_ID, NPCSheet as any, {
+  DocumentSheetConfig.registerSheet(Actor, SYSTEM_ID, NPCSheet as any, {
     types: [ACTOR_DOCUMENT_TYPES[1]],
     makeDefault: true,
     label: 'UESRPG.Sheets.npc',
   });
 
-  Items.registerSheet(SYSTEM_ID, UesrpgItemSheet as any, {
+  DocumentSheetConfig.registerSheet(Item, SYSTEM_ID, UesrpgItemSheet as any, {
     types: [...ITEM_DOCUMENT_TYPES],
     makeDefault: true,
     label: 'UESRPG.Sheets.item',
